9f8672f2db8f404ee70f491831c936025e5b8016
Muhammed <mhammd.best12@gmail.com>
2025-09-26 15:26:18 +0300

    Test commit

--- VueSite/docs/.vuepress/.temp/pages/posts/post2.html.vue
+++
@@ -1,7 +0,0 @@
-<template><div><h1><RouteLink to="/">Muhammed Mustafa</RouteLink></h1>
-<p><RouteLink to="/posts/">/posts</RouteLink> <RouteLink to="/about/">/about</RouteLink></p>
-<p><RouteLink to="/">~</RouteLink> / <RouteLink to="/posts/">posts</RouteLink> / <RouteLink to="/posts/post2.html">Post 2</RouteLink></p>
-<h2>Post 2</h2>
-</div></template>
-
-
