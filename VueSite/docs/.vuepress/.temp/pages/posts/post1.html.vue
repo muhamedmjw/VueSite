63e7e4784cde65746017d8459bdc4637b7f82b3c
Muhammed <mhammd.best12@gmail.com>
2025-09-26 14:50:30 +0300

    Trigger a new workflow commit

--- VueSite/docs/.vuepress/.temp/pages/posts/post1.html.vue
+++ VueSite/docs/.vuepress/.temp/pages/posts/post1.html.vue
@@ -2,7 +2,6 @@
 <p><RouteLink to="/posts/">/posts</RouteLink> <RouteLink to="/about/">/about</RouteLink></p>
 <p><RouteLink to="/">~</RouteLink> / <RouteLink to="/posts/">posts</RouteLink> / <RouteLink to="/posts/post1.html">Post 1</RouteLink></p>
 <h2>Post 1</h2>
-<p>|| Hello ||</p>
 </div></template>
 
 
